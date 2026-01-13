f1792500d72cc812baa4c7f98ae4820a4d46bca2
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-01-14 03:19:46 +0530

    made the file for mini project 1

---
+++ pro1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <dirent.h>
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
+
+int main() {
+
+	
+	exit(EXT_SUCCESS);
+}
