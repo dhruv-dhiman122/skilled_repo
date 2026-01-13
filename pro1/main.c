a128351023002b8a33633819ca7652612bf1d06c
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-01-14 03:43:19 +0530

    added the function for printing the dict element

--- pro1/main.c
+++ pro1/main.c
@@ -1,9 +1,11 @@
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <fcntl.h>
 #include <dirent.h>
+#include <sys/types.h>
 
 //============================ Space for the macro ====================================//
 #define EXT_SUCCESS 0
@@ -16,14 +18,29 @@ pthread_t thread;
 
 //================================ Space for user defined function ===============================//
 
+void* dir_print(void* arg) { // function for printing the element of the dict
+	pthread_mutex_lock(&mutex_lock);
+	const char* FunctionName = (char*) arg;
+	DIR* dir;
+	struct dirent* entry;
+	dir = opendir(FunctionName);
+	while((entry = readdir(dir)) != NULL) {
+		printf("%s\n",entry->d_name);
+	}
+	pthread_mutex_unlock(&mutex_lock);
+	return NULL;
+}
 
 //==================================== Space for the main function ==============================//
 int main(int argv, char* argc[]) {
-	
+	pthread_mutex_init(&mutex_lock, NULL);
+
 	if(argv < 2) { // taking the file name from the user
 		printf("ERR! YOU HAVE TO ENTER THE FILE!!!\n");
 		exit(INPUT_ERR);
 	}
+	
+	const char* user_name = argc[1];
 
 	exit(EXT_SUCCESS);
 }
